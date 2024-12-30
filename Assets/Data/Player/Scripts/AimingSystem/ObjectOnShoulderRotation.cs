a4b2667137fa779d12a68133c7f3d47df1c4eaf2
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2024-12-30 11:44:27 +0100

    Bug de cámara arreglado

--- Assets/Data/Player/Scripts/AimingSystem/ObjectOnShoulderRotation.cs
+++ Assets/Data/Player/Scripts/AimingSystem/ObjectOnShoulderRotation.cs
@@ -5,16 +5,25 @@ public class ObjectOnShoulderRotation : MonoBehaviour
 {
     [SerializeField] private CinemachineOrbitalFollow cam1;
     [SerializeField] private CinemachineOrbitalFollow cam2;
+    [SerializeField] private Transform playerTransform;
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - playerTransform.position;
+    }
 
     private void Update()
     {
+        transform.position = playerTransform.position + offset;
+
         if (cam1.gameObject.activeSelf)
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam1.HorizontalAxis.Value, transform.eulerAngles.z);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, cam1.HorizontalAxis.Value, transform.eulerAngles.z);
         }
         else
         {
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, cam2.HorizontalAxis.Value, transform.eulerAngles.z);
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, cam2.HorizontalAxis.Value, transform.eulerAngles.z);
         }
     }
 }
