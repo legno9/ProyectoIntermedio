4718042bd9973ec0cfd169811a7c0a0312ab1c75
Ubermewtwo <86959459+Ubermewtwo@users.noreply.github.com>
2025-01-12 19:39:51 +0100

    Música de intro del boss level

--- Assets/Data/Boss/BossEntrance/BossDoor.cs
+++ Assets/Data/Boss/BossEntrance/BossDoor.cs
@@ -5,6 +5,7 @@ public class BossDoor : MonoBehaviour
     [SerializeField] private BossController bossController;
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private CanvasGroup bossCanvas;
+    [SerializeField] private AudioSource introMusic;
     private Animator animator;
     private bool bossActive = false;
 
@@ -22,6 +23,7 @@ public class BossDoor : MonoBehaviour
             bossActive = true;
             musicSource.enabled = true;
             bossCanvas.alpha = 1f;
+            introMusic.Stop();
         }
     }
 }
